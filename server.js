0027195a424159cd96128f85c405027f869d71ab
aipoool <comdtvise@gmail.com>
2024-08-01 16:56:35 +0530

    Try5c(Clustering): Limiting the number of workers to 4 ....

--- server.js
+++ server.js
@@ -770,7 +770,7 @@ if (cluster.isPrimary) {
    * FOR NOW , WE SHALL GO TO THE MAX WORKERS
    */
 
-  const numWorkers = Math.min(3, os.cpus().length);
+  const numWorkers = Math.min(4, os.cpus().length);
 
   console.log(`Master ${process.pid} is running`);
   console.log(`Forking ${numWorkers} workers...`);
