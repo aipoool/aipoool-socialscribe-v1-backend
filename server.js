cb7bcfcbb86e20ebf773a2ae3fbcd9346483eadc
aipoool <comdtvise@gmail.com>
2024-08-26 20:46:17 +0530

    chore : Updated the code with extended rate limiting

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ if (process.env.NODE_ENV === "development") {
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minutes
-  max: 200,
+  max: 300,
   message:
     "Too many requests from this IP, please try again after some time--..",
 });
